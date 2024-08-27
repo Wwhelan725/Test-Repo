6c8f5f1c0a0e6033bfc76942fbc2b950f18f38d6
Weber whelan <wwhelan@clemson.edu>
2024-08-27 16:06:49 -0400

    Add files via upload

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        //change the variables from "" to <your name> - <your GitHub username>.
+        //for instance, I would change memberOne to "Matthew Re - Matthewjre"
+        String memberOne = "Caden Allen - coolguycaden";
+        String memberTwo = "Aaron Sprouse - absprouse23";
+        String memberThree = "Ashton McEntarffer - AshtonMcEntarffer";
+        String memberFour = "Weber Whelan - Wwhelan725";
+
+
+        String msg = "Hello World from " + memberOne + ", " + memberTwo + ", " + memberThree + ", " + memberFour;
+
+        System.out.println(msg);
+    }
+}
